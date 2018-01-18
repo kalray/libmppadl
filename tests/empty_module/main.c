8f404d32d871e78f4bdda62a56bfdc1f572decac
Laurent Thévenoux <lthevenoux@kalray.eu>
2018-01-18 11:05:05 +0100

    Adapt empty_module test
    empty_module test runs with libmppadl successfully.
    Tests support (y/n) is as follows:
    
    [y] char_s
    [y] const_char_s
    [y] create_destry_loop
    [y] empty module
    [n] empty_module_unload
    [n] empty_module_unload_free
    [n] global_pointer
    [n] inter_module_call
    [n] inter_module_call_noplt
    [n] load_unload_loop
    [n] mem_func_override
    [n] plt_test
    [n] simple_printf_reloc
    [n] static_const_char_s
    [n] sym_reloc
    [n] three_load_modules

--- tests/empty_module/main.c
+++ tests/empty_module/main.c
@@ -2,29 +2,24 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <mppa_dl_lib.h>
+#include <mppa_dl.h>
 
-extern const char empty_module_mdo[];
+#include "generated/empty_module_hex.h"
 
 int main(int argc, char *argv[])
 {
-  mppa_dl_handle_t *handle;
-  int status = 0;
+	void *handle;
+	int status = 0;
 
-  handle = mppa_dl_handle_new(mppa_dl_this(), 0);
-  if (handle == NULL) {
-    fprintf(stderr, "mppa_dl_handle_new failed\n");
-    status = -1;
-    goto fail;
-  }
-    
-  if (mppa_dl_load_buffer(handle, empty_module_mdo) == -1) {
-    fprintf(stderr, "mppa_dl_load_buffer failed: %s\n", mppa_dl_errstr(handle));
-    status = -1;
-    goto fail;
-  }
-  printf("Module loaded, ok\n");
+	handle = mppa_dl_load(empty_module_mdo, sizeof(empty_module_mdo));
+	if (handle == NULL) {
+		fprintf(stderr, "mppa_dl_load failed: %s\n", mppa_dl_error());
+		status = -1;
+		goto fail;
+	}
 
- fail:
-  return status;
+	printf("Module loaded, ok\n");
+
+fail:
+	return status;
 }
