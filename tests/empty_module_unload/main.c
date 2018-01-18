d748ecaecc73519edcd99e0a2876d3abbe7e1254
Laurent Thévenoux <lthevenoux@kalray.eu>
2018-01-18 11:11:43 +0100

    Adapt empty_module_unload* tests
    This commit adapts the empty_module_unload test for the new
    libmppadl. empty_module_unload_free is removed since it was
    no longer required.

--- tests/empty_module_unload/main.c
+++ tests/empty_module_unload/main.c
@@ -2,37 +2,32 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <mppa_dl_lib.h>
+#include <mppa_dl.h>
 
-extern const char empty_module_unload_mdo[];
+#include "generated/empty_module_unload_hex.h"
 
 int main(int argc, char *argv[])
 {
-  mppa_dl_handle_t *handle;
-  int status = 0;
+	void *handle;
+	int status = 0;
 
-  handle = mppa_dl_handle_new(mppa_dl_this(), 0);
-  if (handle == NULL) {
-    fprintf(stderr, "mppa_dl_handle_new failed\n");
-    status = -1;
-    goto fail;
-  }
-    
-  if (mppa_dl_load_buffer(handle, empty_module_unload_mdo) == -1) {
-    fprintf(stderr, "mppa_dl_load_buffer failed: %s\n", mppa_dl_errstr(handle));
-    status = -1;
-    goto fail;
-  }
-  printf("Module loaded, ok\n");
+	handle = mppa_dl_load(empty_module_unload_mdo, sizeof(empty_module_unload_mdo));
+	if (handle == NULL) {
+		fprintf(stderr, "mppa_dl_load failed: %s\n", mppa_dl_error());
+		status = -1;
+		goto fail;
+	}
 
-  if (mppa_dl_unload(handle) == -1) {
-    fprintf(stderr, "mppa_dl_unload failed: %s\n", mppa_dl_errstr(handle));
-    status = -1;
-    goto fail;
-  }
+	printf("Module loaded, ok\n");
 
-  printf ("Module unloaded correctly\n");
+	if (mppa_dl_unload(handle) == -1) {
+		fprintf(stderr, "mppa_dl_unload failed: %s\n", mppa_dl_error());
+		status = -1;
+		goto fail;
+	}
 
- fail:
-  return status;
+	printf ("Module unloaded correctly\n");
+
+fail:
+	return status;
 }
