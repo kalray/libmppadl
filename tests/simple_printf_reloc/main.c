04e984be3751918b2806f0374c2cc05b1273c2f8
Laurent Thévenoux <lthevenoux@kalray.eu>
2018-01-23 15:10:02 +0100

    Adapt simple_printf_reloc test
    simple_printf_reloc test runs with libmppadl successfully.
    Tests support (y/n) is as follows:
    
    [y] char_s
    [y] const_char_s
    [y] create_destry_loop
    [y] empty module
    [y] empty_module_unload
    [y] global_pointer
    [n] mem_func_override
    [y] plt_test
    [y] simple_printf_reloc
    [y] static_const_char_s
    [n] sym_reloc
    [n] three_load_modules

--- tests/simple_printf_reloc/main.c
+++ tests/simple_printf_reloc/main.c
@@ -3,52 +3,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <mppa_dl_lib.h>
+#include <mppa_dl.h>
 
-extern const char simple_printf_reloc_mdo[];
+#include "generated/simple_printf_reloc_hex.h"
 
 int main(int argc, char *argv[])
 {
-  int status = 0;
-  void *f_ptr, *d_ptr;
+	int status = 0;
+	void *f_ptr, *d_ptr;
 
-  mppa_dl_handle_t *handle;
+	mppa_dl_handle_t *handle;
 
-  handle = mppa_dl_handle_new(mppa_dl_this(), 0);
-  if (handle == NULL) {
-    fprintf(stderr, "mppa_dl_handle_new failed\n");
-    status = -1;
-    goto fail;
-  }
+	handle = mppa_dl_load(simple_printf_reloc_mdo);
+	if (handle == NULL) {
+		fprintf(stderr, "mppa_dl_load failed\n");
+		status = -1;
+		goto fail;
+	}
 
-  if (mppa_dl_load_buffer(handle, simple_printf_reloc_mdo) == -1) {
-    fprintf(stderr, "mppa_dl_load_buffer failed: %s\n", mppa_dl_errstr(handle));
-    status = -1;
-    goto fail;
-  }
+	if ((d_ptr = mppa_dl_sym(handle, "printf_ptr")) != NULL) {
+		printf("Setting printf_ptr to &printf within loaded module\n");
+		*((void **)d_ptr) = (void *)&printf;
+	} else {
+		fprintf(stderr, "Failed to find printf_ptr symbol in dyn object\n");
+		status = -1;
+		goto fail;
+	}
 
-  if ((d_ptr = mppa_dl_sym(handle, "printf_ptr")) != NULL) {
-    printf("Setting printf_ptr to &printf within loaded module\n");
-    *((void **)d_ptr) = (void *)&printf;
-  } else {
-    fprintf(stderr, "Failed to find printf_ptr symbol in dyn object\n");
-    status = -1;
-    goto fail;
-  }
+	if ((f_ptr = mppa_dl_sym(handle, "mppa_dl_main")) == NULL) {
+		fprintf(stderr, "No mppa_dl_main found\n");
+		status = -1;
+		goto fail;
+	} else {
+		printf("Found mppa_dl_main at {{%p}}\n", f_ptr);
+		printf("Module load address is {{%p}}\n", mppa_dl_load_addr(handle));
+		printf("Jumping ! \n");
+		status = ((int (*)(int, char **))f_ptr)(argc, argv);
+	}
 
-  if ((f_ptr = mppa_dl_sym(handle, "mppa_dl_main")) == NULL) {
-    fprintf(stderr, "No mppa_dl_main found\n");
-    status = -1;
-    goto fail;
-  } else {
-    printf("Found mppa_dl_main at {{%p}}\n", f_ptr);
-    printf("Module load address is {{%p}}\n", mppa_dl_load_addr(handle));
-    printf("Jumping ! \n");
-    status = ((int (*)(int, char **))f_ptr)(argc, argv);
-  }
-  
-  printf("Returned status: %d\n", status);
+	printf("Returned status: %d\n", status);
 
- fail:
-  return status;
+fail:
+	return status;
 }
