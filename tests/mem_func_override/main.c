d8d25c022517151e24dac7331496bb568fb17e98
Laurent Thévenoux <lthevenoux@kalray.eu>
2018-01-23 17:25:51 +0100

    Adapt mem_func_override test
    mem_func_override test is now running with libmppadl.
    Testsuite remodeling is complete.

--- tests/mem_func_override/main.c
+++ tests/mem_func_override/main.c
@@ -2,10 +2,10 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <mppa_dl_lib.h>
+#include <mppa_dl.h>
 #include <malloc.h>
 
-extern const char empty_module_mdo[];
+#include "generated/empty_module_hex.h"
 
 static int memalign_called = 0;
 static int malloc_called = 0;
@@ -44,24 +44,21 @@ int main(int argc, char *argv[])
   mppa_dl_handle_t *handle;
   int status = 0;
 
-  mppa_dl_set_mem_funcs(my_mppa_dl_memalign_func, my_mppa_dl_malloc_func, my_mppa_dl_free_func);
+  mppa_dl_set_memalign(my_mppa_dl_memalign_func);
+  mppa_dl_set_malloc(my_mppa_dl_malloc_func);
+  mppa_dl_set_free(my_mppa_dl_free_func);
 
-  handle = mppa_dl_handle_new(mppa_dl_this(), 0);
+  handle = mppa_dl_load(empty_module_mdo);
   if (handle == NULL) {
-    fprintf(stderr, "mppa_dl_handle_new failed\n");
+    fprintf(stderr, "mppa_dl_load failed: %s\n", mppa_dl_error());
     status = -1;
     goto fail;
   }
     
-  if (mppa_dl_load_buffer(handle, empty_module_mdo) == -1) {
-    fprintf(stderr, "mppa_dl_load_buffer failed: %s\n", mppa_dl_errstr(handle));
-    status = -1;
-    goto fail;
-  }
   printf("Module loaded, ok\n");
 
   if (mppa_dl_unload(handle) == -1) {
-    fprintf(stderr, "mppa_dl_unload failed: %s\n", mppa_dl_errstr(handle));
+    fprintf(stderr, "mppa_dl_unload failed: %s\n", mppa_dl_error());
     status = -1;
     goto fail;
   }
