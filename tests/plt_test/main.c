b37e5c7f6bdd3e6d9cd76573e9680c9606a21716
Laurent Thévenoux <lthevenoux@kalray.eu>
2018-01-23 14:03:47 +0100

    Adapt plt_test test
    plt_test is now compatible with new libmppadl. Other valid tests
    have been fixed following libmppadl upgrade.

--- tests/plt_test/main.c
+++ tests/plt_test/main.c
@@ -2,46 +2,40 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <mppa_dl_lib.h>
+#include <mppa_dl.h>
 
-extern const char plt_test_mdo[];
+#include "generated/plt_test_hex.h"
 
 // this function should be used by the dynamic module
-int self_contained_magic_func_increment_used_for_reloc(int arg){
-  return arg + 1;
+int self_contained_magic_func_increment_used_for_reloc(int arg) {
+	return arg + 1;
 }
 
 int main(int argc, char *argv[])
 {
-  mppa_dl_handle_t *handle;
-  int status = 0;
-  void *f_ptr;
+	void *handle;
+	int status = 0;
+	void *f_ptr;
 
-  handle = mppa_dl_handle_new(mppa_dl_this(), 0);
-  if (handle == NULL) {
-    fprintf(stderr, "mppa_dl_handle_new failed\n");
-    status = -1;
-    goto fail;
-  }
-    
-  if (mppa_dl_load_buffer(handle, plt_test_mdo) == -1) {
-    fprintf(stderr, "mppa_dl_load_buffer failed: %s\n", mppa_dl_errstr(handle));
-    status = -1;
-    goto fail;
-  }
+	handle = mppa_dl_load(plt_test_mdo);
+	if (handle == NULL) {
+		fprintf(stderr, "mppa_dl_load failed: %s\n", mppa_dl_error());
+		status = -1;
+		goto fail;
+	}
   
-  if ((f_ptr = mppa_dl_sym(handle, "mppa_dl_main")) == NULL) {
-    fprintf(stderr, "No mppa_dl_main found in plt_test module\n");
-    goto fail;
-  } else {
-    printf("Found mppa_dl_main in plt_test module at {{%p}}\n",  f_ptr);
-    printf("Module load address is {{%p}}\n", mppa_dl_load_addr(handle));
+	if ((f_ptr = mppa_dl_sym(handle, "mppa_dl_main")) == NULL) {
+		fprintf(stderr, "No mppa_dl_main found in plt_test module\n");
+		goto fail;
+	} else {
+		printf("Found mppa_dl_main in plt_test module at {{%p}}\n",  f_ptr);
+		printf("Module load address is {{%p}}\n", mppa_dl_load_addr(handle));
 
-    status = ((int (*)(int, char **))f_ptr)(argc, argv);
-  }
+		status = ((int (*)(int, char **))f_ptr)(argc, argv);
+	}
 
-  printf("Returned status: %d\n", status);
+	printf("Returned status: %d\n", status);
 
- fail:
-  return status;
+fail:
+	return status;
 }
