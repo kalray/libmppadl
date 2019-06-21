5a106cb77fb65db6ed2785506383810f15c7f9c5
Marc Poulhiès <marc.poulhies@kalray.eu>
2019-06-21 14:08:12 +0200

    Fix K1_32, K1_64 and K1_RELATIVE relocations handling
    Absolute relocations are incorrectly handled as RELATIVE relocations.
    ref T9928
    
    Differential Revision: D1244

--- src/arch/k1c.c
+++ src/arch/k1c.c
@@ -4,6 +4,15 @@
 
 #include "mppa_dl_load.h"
 
+/* Macros used for relocations that depends on target mode. */
+#ifdef __K1_64__
+#define R_TMP tmp64
+#define R_EWORD ElfK1_Lword
+#else
+#define R_TMP tmp
+#define R_EWORD ElfK1_Word
+#endif
+
 /* Disable warning: the casting from/to an int to/from a pointer
  * always triggers the error. */
 #pragma GCC diagnostic push
@@ -26,35 +35,45 @@ int mppa_dl_apply_rela(mppa_dl_handle_t *hdl, ElfK1_Rela rel)
 	switch (ELFK1_R_TYPE(rel.r_info)) {
 	case R_K1_NONE:
 		break;
-	case R_K1_32:
-	case R_K1_RELATIVE:
-	        tmp = ((ElfK1_Word)rel.r_addend + (uintptr_t) hdl->addr);
-		memcpy((void*)((uintptr_t)hdl->addr + rel.r_offset), &tmp,
-			sizeof(ElfK1_Word));
-		MPPA_DL_LOG(2, ">> Wrote 0x%lx at 0x%lx\n",
-			    (long unsigned)tmp, (long unsigned) ((uintptr_t)hdl->addr + rel.r_offset));
 
-		break;
-	case R_K1_GLOB_DAT:
-	case R_K1_JMP_SLOT:
-	  tmp = (uintptr_t) mppa_dl_sym_lookup(hdl, &hdl->strtab[sym.st_name], 0);
+	case R_K1_32:
+		tmp = (uintptr_t) mppa_dl_sym_lookup(hdl, &hdl->strtab[sym.st_name], 0);
 		if (tmp == 0 && mppa_dl_errno_get_status() != E_NONE)
 			return -2;
-
+		tmp += (ElfK1_Word)rel.r_addend;
 		memcpy((void *)hdl->addr + rel.r_offset, &tmp,
-			sizeof(ElfK1_Word));
+		       sizeof(tmp));
 		break;
 
 	case R_K1_64:
-	case R_K1_RELATIVE64:
-	        tmp64 = ((ElfK1_Lword)rel.r_addend + (uintptr_t) hdl->addr);
-		memcpy((void*)((uintptr_t)hdl->addr + rel.r_offset), &tmp64,
-			sizeof(ElfK1_Lword));
+		tmp64 = (uintptr_t) mppa_dl_sym_lookup(hdl, &hdl->strtab[sym.st_name], 0);
+		if (tmp64 == 0 && mppa_dl_errno_get_status() != E_NONE)
+			return -2;
+		tmp64 += (ElfK1_Lword)rel.r_addend;
+		memcpy((void *)hdl->addr + rel.r_offset, &tmp64,
+		       sizeof(tmp64));
+		break;
+
+	/* The following 3 relocations will act on 32bits or 64bits, depending on the target */
+	case R_K1_RELATIVE:
+	        R_TMP = ((R_EWORD)rel.r_addend + (uintptr_t) hdl->addr);
+		memcpy((void*)((uintptr_t)hdl->addr + rel.r_offset), &R_TMP,
+			sizeof(R_EWORD));
 		MPPA_DL_LOG(2, ">> Wrote 0x%lx at 0x%lx\n",
-			    (long unsigned)tmp64, (long unsigned) ((uintptr_t)hdl->addr + rel.r_offset));
+			    (long unsigned)R_TMP, (long unsigned) ((uintptr_t)hdl->addr + rel.r_offset));
 
 		break;
 
+	/* We are not implementing any lazy binding, so treat JMP_SLOT as GLOB_DAT. */
+	case R_K1_GLOB_DAT:
+	case R_K1_JMP_SLOT:
+		R_TMP = (uintptr_t) mppa_dl_sym_lookup(hdl, &hdl->strtab[sym.st_name], 0);
+		if (R_TMP == 0 && mppa_dl_errno_get_status() != E_NONE)
+			return -2;
+
+		memcpy((void *)hdl->addr + rel.r_offset, &R_TMP,
+			sizeof(R_EWORD));
+		break;
 
 	default:
 		MPPA_DL_LOG(2, ">> Unhandled relocation type %ld\n", rel.r_info);
