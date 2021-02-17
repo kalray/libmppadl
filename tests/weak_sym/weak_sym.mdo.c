a242b43c9881de5a747653ec63e2b3d7a094deb6
jhascoet <jhascoet@kalray.eu>
2021-02-17 13:39:06 +0100

    Fix NULL weak symbol test
    Summary:
    The test was dereferencing a
    NULL pointer; thus, we fix it.
    Prior to this patch, at address zero,
    zero values were stored, that made this
    test work.
    
    Test Plan: mppadl
    
    Reviewers: O24 libmppadl!
    
    Differential Revision: https://phab.kalray.eu/D11218

--- tests/weak_sym/weak_sym.mdo.c
+++ tests/weak_sym/weak_sym.mdo.c
@@ -1,5 +1,5 @@
 // expected value 0
-extern int *this_is_weak_and_should_be_0 __attribute__((weak));
+extern int this_is_weak_and_should_be_0 __attribute__((weak));
 
 // expected value != NULL and pointing to 0xdeadbeef
 extern int *this_is_weak_but_bound_to_main __attribute__((weak));
@@ -15,7 +15,7 @@ extern char this_is_weak_bound_to_main_from_cli __attribute__((weak));
 int
 mppa_dl_main(int argc, char *argv[])
 {
-  return !((this_is_weak_and_should_be_0 == 0)
+  return !((&this_is_weak_and_should_be_0 == 0)
 	   && &this_is_weak_bound_to_main_from_cli_nonull == (void*)0xcafebebe
 	   && &this_is_weak_bound_to_main_from_cli == 0
 	   && (this_is_weak_but_bound_to_main != 0 && *this_is_weak_but_bound_to_main == 0xdeadbeef));
